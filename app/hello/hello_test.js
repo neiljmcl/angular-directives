c9bc8bd0e1dc6954b0f9d69321cd109106644195
neiljmcl <neiljmcl@gmail.com>
2016-04-30 13:56:25 +0100

    We have a serious, albeit trivial directive test

--- app/hello/hello_test.js
+++ app/hello/hello_test.js
@@ -1,12 +1,20 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.hello module', function() {
+  var $compile;
+  var $rootScope;
 
   beforeEach(module('myApp.hello'));
+  beforeEach(inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
 
-  describe('hello directive', function(){
-    it('should fail', inject(function() {
-      expect(true).toBe(false);
-    }));
+
+  describe('hello directive', function() {
+    it('should say hello to ted', function() {
+      var element = $compile('<hello></hello>')($rootScope);
+      expect(element.text()).toBe("hello there ted");
+    });
   });
 });
